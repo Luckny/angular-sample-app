cec9934022bb626fd787069aea2a7f65d00418c4
Luckny <93001262+Luckny@users.noreply.github.com>
2022-07-05 15:14:04 -0400

    error, updating cli

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,29 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
   public getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   public getHero(id: number): Observable<Hero> {
     const hero = HEROES.find((h) => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
