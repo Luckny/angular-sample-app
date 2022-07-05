0ad8ea636f58d3cb26bd21832e0bec0441f6134e
Luckny <93001262+Luckny@users.noreply.github.com>
2022-07-05 13:30:17 -0400

    step 5: add Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,13 +16,6 @@ export class HeroesComponent implements OnInit {
 
   public heroes: Hero[] = [];
 
-  public selectedHero?: Hero;
-
-  public onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id ${hero.id}`);
-  }
-
   private getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
