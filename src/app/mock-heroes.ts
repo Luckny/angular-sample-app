f15b115383ca9823970b228aa7917c7e1deb895d
Luckny <93001262+Luckny@users.noreply.github.com>
2022-07-13 08:55:55 -0400

    first commit - Build the form

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,23 @@
 import { Hero } from './hero';
 
+const powers: string[] = [
+  'Really Smart',
+  'Super Flexible',
+  'Super Hot',
+  'Weather Changer',
+];
+const rand = () => {
+  return Math.floor(Math.random() * 3);
+};
+
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Dr. Nice', power: powers[rand()] },
+  { id: 13, name: 'Bombasto', power: powers[rand()] },
+  { id: 14, name: 'Celeritas', power: powers[rand()] },
+  { id: 15, name: 'Magneta', power: powers[rand()] },
+  { id: 16, name: 'RubberMan', power: powers[rand()] },
+  { id: 17, name: 'Dynama', power: powers[rand()] },
+  { id: 18, name: 'Dr. IQ', power: powers[0] },
+  { id: 19, name: 'Magma', power: powers[rand()] },
+  { id: 20, name: 'Tornado', power: powers[rand()] },
 ];
